ce525e66040aa7813df112935211ef548cf1f41f
Jesus Prieto Colomina <chus1818@gmail.com>
2015-06-22 22:46:22 +0100

    Tests added for binaries and strings 6

--- strings_and_binaries_6.exs
+++ strings_and_binaries_6.exs
@@ -7,7 +7,7 @@ defmodule Capitalize do
   end
 
   defp _capitalize_sentences(<<>>, converted_string) do
-    IO.puts converted_string
+    converted_string
   end
   defp _capitalize_sentences(<< 46, 32, head, tail :: binary >>, converted_string) do
     _capitalize_sentences(tail, converted_string <> << 46, 32 >> <> << _upcase(head) >>)
@@ -29,3 +29,14 @@ defmodule Capitalize do
     ascii_char
   end
 end
+
+ExUnit.start
+
+defmodule CapitalizeTest do
+  use ExUnit.Case, async: true
+
+  test "it works for a complicated string" do
+    assert Capitalize.capitalize_sentences("sOme Com. plic. AtEd stRing") == "Some com. Plic. Ated string"
+  end
+end
+
